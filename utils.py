19a60e76fec5a1c65347ef27bdb003ecc4e42cec
Yanis <yanis.bassobert@ecl17.ec-lyon.fr>
2020-11-09 14:33:18 +0100

    Fix un-normalization function

--- utils.py
+++ utils.py
@@ -228,6 +228,17 @@ def showImgFromTensor(tensor):
         plt.imshow(img)
         plt.axis('off')
     plt.show()
+    
+def unormalize(tensor,mean,std):
+    for t, m, s in zip(tensor, mean, std):
+            t.mul_(s).add_(m)
+    return tensor
+
+def reverse_transform(tensor):
+  tensor = unormalize(tensor,mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+  image = tensor.numpy()
+  image = image.transpose((1, 2, 0))
+  return image
 
 def visualizePrediction(model,dataloader):
 
