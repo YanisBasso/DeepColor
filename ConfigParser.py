060fd9ad4673a3e992595ea6ddd29163a254677b
Yanis <yanis.bassobert@ecl17.ec-lyon.fr>
2020-11-12 10:01:44 +0100

    Add function parser

--- ConfigParser.py
+++ ConfigParser.py
@@ -12,6 +12,7 @@ import json
 from pathlib import Path
 from datetime import datetime 
 from collections import OrderedDict
+from functools import partial 
 
 class ConfigParser:
     
@@ -42,6 +43,20 @@ class ConfigParser:
         assert all([k not in module_args for k in kwargs]), 'Overwriting kwargs given in config file is not allowed'
         module_args.update(kwargs)
         return getattr(module, module_name)(*args, **module_args)
+    
+    def init_ftn(self, name, module, *args, **kwargs):
+        """
+        Finds a function handle with the name given as 'type' in config, and returns the
+        function with given arguments fixed with functools.partial.
+        `function = config.init_ftn('name', module, a, b=1)`
+        is equivalent to
+        `function = lambda *args, **kwargs: module.name(a, *args, b=1, **kwargs)`.
+        """
+        module_name = self[name]['type']
+        module_args = dict(self[name]['args'])
+        assert all([k not in module_args for k in kwargs]), 'Overwriting kwargs given in config file is not allowed'
+        module_args.update(kwargs)
+        return getattr(module, module_name), *args, **module_args)
     
     @classmethod
     def from_args(cls,args):
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ Created on Mon Nov  2 15:08:35 2020
 
 from ConfigParser import ConfigParser
 import argparse
+import torch
+import models
 
 # Ignore warnings
 import warnings
@@ -22,8 +24,21 @@ if __name__ == '__main__':
                       help='path to latest checkpoint (default: None)')
     args.add_argument('-d', '--device', default=None, type=str,
                       help='indices of GPUs to enable (default: all)')
-
+    
+    #create a config parser 
     config = ConfigParser.from_args(args)
+    
+    # setup data_loader instances
+    
+    # build model architecture
+    model_ft,input_size = config.init_ftn('arch',models)
+    print(model_ft)
+
+    # get function handles of loss and metrics
+    
+    # build optimizer
+    optimizer = config.init_obj('optimizer', torch.optim, model_ft.parameters())
+    print(optimizer)
 
 
 
