374dc2da37d174580371e36323e50cf433aceb17
Yanis <yanis.bassobert@ecl17.ec-lyon.fr>
2020-12-28 09:25:22 +0100

    Add penalised loss

--- loss.py
+++ loss.py
@@ -7,11 +7,13 @@ Created on Mon Dec 21 15:02:35 2020
 """
 
 
-import torch
-
-
 def mse_loss(input, target):
     return ((input - target) ** 2).mean()
 
 def weighted_mse_loss(input, target, weight):
     return (weight * (input - target) ** 2).mean()
+
+def weighted_mse_loss2(input, target, weight,alpha=0.5):
+    loss = mse_loss(input, target)
+    penalisation  = alpha*weighted_mse_loss(input, target, weight)
+    return loss + penalisation
